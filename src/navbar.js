2f5461f9068d578fa8ac303b108a78f64192ee42
Sheshmani Patel <sumitkumar47581@gmail.com>
2023-06-19 12:43:43 +0530

    Manking a navbar.js

---
+++ src/navbar.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+class Navbar extends React.Component{
+    render(){
+        return(
+            <>
+            <div>
+                <div>
+                    <h1>Navbar</h1>
+                </div>
+                <div>
+                    <img alt="cart icon" />
+                    <span>3</span>                    
+                </div>
+            </div>
+            
+            </>
+        )
+    }
+}
+export default Navbar;
