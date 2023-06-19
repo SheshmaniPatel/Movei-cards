4d9aa09c037dc38bc91f59ef8f3998f06b5f5734
Sheshmani Patel <sumitkumar47581@gmail.com>
2023-06-19 12:45:17 +0530

    Adding key for the cards

--- src/MovieList.js
+++ src/MovieList.js
@@ -7,6 +7,7 @@ class MovieList extends React.Component {
     this.state = {
       movies: [
         {
+          id:1,
           title: "Avatar",
           plot: "Totally icredeble 3D moveie ever , fun , journey , VFX .",
           price: 399,
@@ -15,9 +16,10 @@ class MovieList extends React.Component {
           rating: 8.9,
           starscount: 0,
           fav: false,
-          cart: false,
+          cart: true,
         },
         {
+          id:2,
           title: "Avengers",
           plot: "Earth's mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity",
           price: 299,
@@ -26,9 +28,10 @@ class MovieList extends React.Component {
           rating: 7.9,
           starscount: 0,
           fav: false,
-          cart: false,
+          cart: true,
         },
         {
+          id:3,
           title: "Thor the dark world",
           plot: "A hero who have the power of strength,stamina, fly with lightspeed,and sharp mind",
           price: 249,
@@ -37,7 +40,7 @@ class MovieList extends React.Component {
           rating: 8.9,
           starscount: 0,
           fav: false,
-          cart: false,
+          cart: true,
         },
       ],
     };
@@ -103,6 +106,7 @@ class MovieList extends React.Component {
             removeStars={this.handleDecStar}
             toogleFav={this.handleToggleFav}
             toogleCart={this.handleToggleCart}
+            key={movie.id}
           />
         ))}
       </>
