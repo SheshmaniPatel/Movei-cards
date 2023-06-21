d1797c4c9a451d93960f2eb30c5c4c9aa50da89b
Sheshmani Patel <sumitkumar47581@gmail.com>
2023-06-21 12:58:01 +0530

    making a file moviedata and calling its data in movielist

--- src/MovieList.js
+++ src/MovieList.js
@@ -1,60 +1,12 @@
 import MovieCard from "./MovieCard";
 import React from "react";
+import {movies} from "./moviesdata"
 
 class MovieList extends React.Component {
   constructor() {
     super();
     this.state = {
-      movies: [
-        {
-          id: 1,
-          title: "Avatar",
-          plot: "Totally icredeble 3D moveie ever , fun , journey , VFX .",
-          price: 399,
-          poster:
-            "https://posterspy.com/wp-content/uploads/2022/01/Avatar-The-Way-Of-Water.jpg",
-          rating: 8.9,
-          starscount: 0,
-          fav: false,
-          cart: true,
-        },
-        {
-          id: 2,
-          title: "Avengers",
-          plot: "Earth's mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity",
-          price: 299,
-          poster:
-            "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg",
-          rating: 7.9,
-          starscount: 0,
-          fav: false,
-          cart: true,
-        },
-        {
-          id: 3,
-          title: "Thor Love and Thunder",
-          plot: "A hero who have the power of strength,stamina, fly with lightspeed,and sharp mind",
-          price: 349,
-          poster:
-            " https://cdn.marvel.com/content/1x/bigsalad_genavailposter_pay1_presunrise_v3_lg.jpg",
-          rating: 9.9,
-          starscount: 0,
-          fav: false,
-          cart: true,
-        },
-        {
-          id: 4,
-          title: " SPIDER-MAN No Way Home",
-          plot: "A hero who have the power of spider,and have the sixth sense with great humor !",
-          price: 299,
-          poster:
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrdPsGJEBxBev7gKo_EMp0Pgk7Q7su_xTUxf3vo8dE9S_CiG2Z",
-          rating: 7.9,
-          starscount: 0,
-          fav: false,
-          cart: true,
-        },
-      ],
+      movies: movies
     };
   }
 
